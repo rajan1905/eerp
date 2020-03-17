1f9066608193e9365bbeff949d4706d171f356a7
rajan_1905@live.com <rajan_1905@live.com>
2020-03-17 22:20:18 +0000

    setup

---
+++ src/main/java/com/rp/erp/eduerp/EduerpApplication.java
@@ -0,0 +1,13 @@
+package com.rp.erp.eduerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduerpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rp/erp/eduerp/EduerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rp.erp.eduerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
